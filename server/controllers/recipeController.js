1b342f86476abf9d7b4d6436dd4b19ec3da77c00
anushka9221 <anushkaghorpade0921@gmail.com>
2022-05-19 00:38:41 +0530

    123

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -240,40 +240,40 @@ exports.submitRecipeOnPost = async (req, res) => {
 
 
 
-// async function insertDummyCategoryData(){
-//   try{
-//     await Category.insertMany([
-//       {
-//         "name":"Thai",
-//         "image":"thai-food.jpg"
-//       },
-//       {
-//         "name":"American",
-//         "image":"american-food.jpg"
-//       },
-//       {
-//         "name":"Chinese",
-//         "image":"chinese-food.jpg"
-//       },
-//       {
-//         "name":"Mexican",
-//         "image":"mexican-food.jpg"
-//       },
-//       {
-//         "name":"Indian",
-//         "image":"indian-food.jpg"
-//       },
-//       {
-//         "name":"Spanish",
-//         "image":"spanish-food.jpg"
-//       },
-//     ]);
-//   }catch(error){
-//   console.log('err',+error)
-// }
-// }
-//
-// insertDummyCategoryData();
+async function insertDummyCategoryData(){
+  try{
+    await Category.insertMany([
+      {
+        "name":"Thai",
+        "image":"thai-food.jpg"
+      },
+      {
+        "name":"American",
+        "image":"american-food.jpg"
+      },
+      {
+        "name":"Chinese",
+        "image":"chinese-food.jpg"
+      },
+      {
+        "name":"Mexican",
+        "image":"mexican-food.jpg"
+      },
+      {
+        "name":"Indian",
+        "image":"indian-food.jpg"
+      },
+      {
+        "name":"Spanish",
+        "image":"spanish-food.jpg"
+      },
+    ]);
+  }catch(error){
+  console.log('err',+error)
+}
+}
+
+insertDummyCategoryData();
 //
 //
 //
